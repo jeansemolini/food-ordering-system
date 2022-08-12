81ad43d38f86936edea7d86dd3c945bc8fdeb924
Jean Semolini <jean.maiko@hotmail.com>
2022-08-12 18:10:35 -0300

    Adicionando value objects e domain event

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -0,0 +1,54 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+public class Money {
+    private final BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public boolean isGreaterThanZero() {
+        return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isGreater(Money money) {
+        return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
+    }
+
+    public Money add(Money money) {
+        return new Money(setScala(this.amount.add(money.getAmount())));
+    }
+
+    public Money substract(Money money) {
+        return new Money(setScala(this.amount.subtract(money.getAmount())));
+    }
+
+    public Money substract(int multiplier) {
+        return new Money(setScala(this.amount.multiply(new BigDecimal(multiplier))));
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount.equals(money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    private BigDecimal setScala(BigDecimal input) {
+        return input.setScale(2, RoundingMode.HALF_EVEN);
+    }
+}
